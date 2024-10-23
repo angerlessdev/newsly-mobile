74d20ef6bf208a81fc16e160001730497d12e94c
Angel Cancho <u201721995@upc.edu.pe>
2024-10-22 23:16:00 -0500

    refactor(home): marks _pages list as const to optimize widget rebuilding

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [SearchNewsPage(), const Icon(Icons.favorite)];
+  final List<Widget> _pages = const [SearchNewsPage(), Icon(Icons.favorite)];
 
   void _onTapPressed(index) {
     setState(() {
