8aea84b7ed1107010671b260ac74f2a1dfd48176
Angel Cancho <u201721995@upc.edu.pe>
2024-10-23 03:03:53 -0500

    feat(favorites): add FavoriteNewsPage

---
+++ lib/favorite_news_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoriteNewsPage extends StatefulWidget {
+  const FavoriteNewsPage({super.key});
+
+  @override
+  State<FavoriteNewsPage> createState() => _FavoriteNewsPageState();
+}
+
+class _FavoriteNewsPageState extends State<FavoriteNewsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
