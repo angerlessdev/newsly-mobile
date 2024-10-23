c7a3bd7db25796548e0b45ef72938931c75ee343
Angel Cancho <u201721995@upc.edu.pe>
2024-10-23 12:35:07 -0500

    feat(favorites): add functionality to remove favorite news items

--- lib/favorite_news_page.dart
+++ lib/favorite_news_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:newsly/news.dart';
+import 'package:newsly/utils.dart';
 
 class FavoriteNewsPage extends StatefulWidget {
   const FavoriteNewsPage({super.key});
@@ -8,8 +10,94 @@ class FavoriteNewsPage extends StatefulWidget {
 }
 
 class _FavoriteNewsPageState extends State<FavoriteNewsPage> {
+  final TextEditingController _controller = TextEditingController();
+  List<News> _news = [];
+
+  void loadData() async {
+    List maps = await loadJsonFromAssets('assets/articles.json');
+    setState(() {
+      _news = maps.map((map) => News.fromJson(map)).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //double width = MediaQuery.sizeOf(context).width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onSubmitted: (value) {},
+            controller: _controller,
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)))),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _news.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        _news[index].urlToImage,
+                        width: double.infinity,
+                        height: 150,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const SizedBox(
+                              width: double.infinity,
+                              height: 150,
+                              child:
+                                  Center(child: Text('Image failed to load')));
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _news.removeAt(index);
+                                });
+                              },
+                              icon: const Icon(Icons.remove_circle_outline)),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          _news[index].title,
+                          maxLines: 1,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(_news[index].author),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
   }
 }
