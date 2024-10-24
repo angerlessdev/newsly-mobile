6943fbdc9e40c16851746d62870ad408bb8d80fc
Angel Cancho <u201721995@upc.edu.pe>
2024-10-24 02:54:10 -0500

    feat(search): update search functionality and improve UI

--- lib/search_news_page.dart
+++ lib/search_news_page.dart
@@ -37,7 +37,9 @@ class _SearchNewsPageState extends State<SearchNewsPage> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: TextField(
-            onSubmitted: (value) {},
+            onSubmitted: (value) {
+              loadData();
+            },
             controller: _controller,
             decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.search),
@@ -64,18 +66,21 @@ class _SearchNewsPageState extends State<SearchNewsPage> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.network(
-                          _news[index].urlToImage,
-                          width: double.infinity,
-                          height: 150,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return const SizedBox(
-                                width: double.infinity,
-                                height: 150,
-                                child: Center(
-                                    child: Text('Image failed to load')));
-                          },
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            _news[index].urlToImage,
+                            width: double.infinity,
+                            height: 150,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const SizedBox(
+                                  width: double.infinity,
+                                  height: 150,
+                                  child: Center(
+                                      child: Text('Image failed to load')));
+                            },
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.all(4.0),
@@ -86,8 +91,13 @@ class _SearchNewsPageState extends State<SearchNewsPage> {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.all(4.0),
+                          padding: const EdgeInsets.only(
+                              left: 4.0, right: 4.0, bottom: 4.0),
                           child: Text(_news[index].author),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(_news[index].description, maxLines: 3),
                         )
                       ],
                     ),
