492bedf6f90baf02d64790c9054e3ea1fdd21628
Angel Cancho <u201721995@upc.edu.pe>
2024-10-24 21:55:30 -0500

    feat: add NewsState classes for news loading states

---
+++ lib/presentation/bloc/news_state.dart
@@ -0,0 +1,17 @@
+import 'package:newsly/domain/news.dart';
+
+abstract class NewsState {}
+
+class NewsInitialState extends NewsState {}
+
+class NewsLoadingState extends NewsState {}
+
+class NewsLoadedState extends NewsState {
+  final List<News> news;
+  NewsLoadedState({required this.news});
+}
+
+class NewsErrorState extends NewsState {
+  final String errorMessage;
+  NewsErrorState({required this.errorMessage});
+}
